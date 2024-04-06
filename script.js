74c40835fb0c0ed6a6e0f4b72c15dba32ebbf9f2
ahad1512 <139266271+ahad1512@users.noreply.github.com>
2024-04-07 01:38:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let val_1 = prompt("Enter value 1");
+let val_2 = prompt("Enter value 2");
+
+let num1 = parseInt(val_1);
+let num2 = parseInt(val_2);
+
+let result = num1*2 + num2;
+ alert("The final result is: " + result)
+
